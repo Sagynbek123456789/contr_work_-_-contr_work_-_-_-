effae6d723e3553c7a3866b47be0a3eb0cf490cb
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-09 16:26:31 +0600

    внес дополнительные изменения которые небходимы для проекта

--- book/webapp/forms.py
+++ book/webapp/forms.py
@@ -6,7 +6,7 @@ class TaskForm(forms.Form):
     author = forms.CharField(max_length=60, required=True, label='автор')
     mail = forms.EmailField(max_length=70, required=True, label='почта')
     description = forms.CharField(max_length=50, required=True, label='описание')
-    # status = forms.ChoiceField(choices=status_choices, label='Статус')
+
 
 
 class AuthorSearchForm(forms.Form):
