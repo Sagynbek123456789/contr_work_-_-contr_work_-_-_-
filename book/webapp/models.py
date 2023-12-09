6adec81fc0111763a31b0f6b424b105d19e5e989
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-09 12:11:17 +0600

    Создал модель с заданными условиями согласно заданию

--- book/webapp/models.py
+++ book/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+status_choices = [('active', 'Активно'), ('blocked', 'В Заблокировано')]
+
+
 # Create your models here.
+class Task(models.Model):
+    author = models.CharField(max_length=60, null=False, blank=False, verbose_name='автор')
+    mail = models.EmailField(max_length=70, null=False, blank=False, verbose_name='почта')
+    description = models.TextField(max_length=50, null=False, blank=False, verbose_name='описание')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=status_choices, default='active',
+                              verbose_name='Статус')
+
+    def __str__(self):
+        return f"{self.author} - {self.description[:50]}"
+
+
